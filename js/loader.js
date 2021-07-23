7d2fcf68d602cfa7cf63e053b89999875faa4455
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2021-07-23 19:32:51 -0300

    Improving the dinamic template loader

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,19 @@
-function loadBody(source, title){
+function loadBody(source){
     var xhr= new XMLHttpRequest();
     xhr.open('GET', source, true);
     xhr.onreadystatechange= function() {
         if (this.readyState !== 4) return;
         if (this.status !== 200) return;
-        document.getElementById('page-title').innerHTML = title;
-        document.getElementById('page-content').innerHTML = this.responseText;
+
+        const new_doc = document.implementation.createHTMLDocument("newDocument").documentElement;
+        new_doc.innerHTML = this.responseText;
+        
+        var title = new_doc.querySelector('.title');
+        var body = new_doc.querySelector('.body');
+
+        if (title) document.querySelector('.title').textContent = title.textContent;
+        if (body) document.querySelector('.body').innerHTML = body.innerHTML;
     };
+
     xhr.send();
 }
